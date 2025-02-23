7f838c7e495d162b4ca1167ab67257c6c9f07bbe
Halit Enes Kalaycı <halitenes.kalayci@gmail.com>
2025-02-23 16:17:41 +0300

    if block

--- src/index.ts
+++ src/index.ts
@@ -26,3 +26,42 @@ let students2:string[] = students; // COPY-> Shallow copy,deep copy
 students2.push("Deniz");
 console.log(students)
 console.log(students2)
+
+// karar blokları
+let age:number=18;
+
+// koşul ve koşullar
+// Bir koşul bloğu yalnızca bir karar verir.
+
+// if,
+// if,else
+// if,else if,else
+if (age >= 18)
+{
+    // Scope kavramı -> Bir bloğun kapsadığı alanın belirlenmesi.
+    console.log("Kullanıcı reşit");
+}
+if(age == 18)
+{
+    console.log("Kullanıcı tam 18 yaşında.")
+}
+else // Yukarıdaki koşul-(lar) sağlanmaması durumunda çalışsın.
+{
+    console.log("Kullanıcı reşit değil..")
+}
+
+console.log("İf bitti.")
+
+//
+
+let gender:string="E";
+if(age >= 18 && gender == "E") // VE => true,true => true
+{
+    console.log("Kullanıcı 18 yaşından büyük bir erkek")
+}
+
+if(age>=18 || gender == "K")// VEYA => true,false -> true
+{
+    console.log("Kullanıcı kadın ya da 18 yaşından büyük")
+} 
+
